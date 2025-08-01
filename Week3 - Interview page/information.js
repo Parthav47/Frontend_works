9d395a1e74697a28a87d2bec9b69e63597a8542d
Parthav47 <parthavudayasankar@gmail.com>
2025-08-01 17:50:09 +0530

    Add files via upload

---
+++ Week3 - Interview page/information.js
@@ -0,0 +1,33 @@
+const data =[ {
+    title: 'Web Development /b Intenship Program',
+    Duration:'1 month/2 months',
+    Location : 'Virtual',
+    Stipend:'-',
+    StartDate :'01 June 2024',
+    link: '#'
+  },
+{
+    title: 'App Development /b Intenship Program',
+    Duration:'1 month/2 months',
+    Location : 'Virtual',
+    Stipend:'-',
+    StartDate :'01 June 2024',
+    link: '#'
+  },
+{
+    title: 'Python Programming /b Intenship Program',
+    Duration:'1 month/2 months',
+    Location : 'Virtual',
+    Stipend:'-',
+    StartDate :'01 June 2024',
+    link: '#'
+  },
+{
+   title: 'Java Programming /b Intenship Program',
+   Duration:'1 month/2 months',
+   Location : 'Virtual',
+   Stipend:'-',
+   StartDate :'01 June 2024',
+   link: '#'
+  }
+ ]  
